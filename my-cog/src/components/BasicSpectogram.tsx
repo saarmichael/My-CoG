bd208e1b84b21efbfab9793d5bfa30f3e0b7b273
Msaar <saarmichael@gmail.com>
2023-01-19 16:26:37 +0200

    created my own data and colors for the spectogram

--- my-cog/src/components/BasicSpectogram.tsx
+++ my-cog/src/components/BasicSpectogram.tsx
@@ -3,48 +3,57 @@ import { lightningChart, PalettedFill, LUT, ColorRGBA, emptyLine, Themes } from 
 import { createWaterDropDataGenerator } from "@arction/xydata";
 
 
-
-
-
-
 const BasicSpectogram = () => {
 
-    // Create a new function 
-    const generateData = () => {
+    /* asynchronous function that generate resolutionX over resolutionY matrix of random numbers */
+    const generateData = async (resolutionX: number, resolutionY: number) => {
+        // create a new number matrix of 100 over 100
+        const data: number[][] = []
+        // loop over the matrix and fill it with random numbers
+        for (let i = 0; i < resolutionX; i++) {
+            let row = []
+            for (let j = 0; j < resolutionY; j++) {
+                row.push(Math.random() * 100)
+            }
+            data.push(row)
+        }
+        return data
+
+    }
+
+    /* funciton that creates the chart and assigns the data to it */
+    const generateChart = () => {
         const resolutionX = 1000;
         const resolutionY = 1000;
 
         // Create a XY Chart.
         const chart = lightningChart()
             .ChartXY({
-                theme: Themes.darkGold,
+                theme: Themes.darkGold
             })
             .setTitle(
                 `Heatmap Grid Series ${resolutionX}x${resolutionY} (${(
                     (resolutionX * resolutionY) /
-                    1000000
+                    10000
                 ).toFixed(1)} million data points)`
             ).setPadding({ right: 40 })
 
+        // Create a color look up table (LUT) for the heatmap.
         const palette = new LUT({
-            units: "intensity",
             steps: [
-                { value: 0, color: ColorRGBA(255, 255, 0) },
-                { value: 30, color: ColorRGBA(255, 204, 0) },
-                { value: 45, color: ColorRGBA(255, 128, 0) },
-                { value: 60, color: ColorRGBA(255, 0, 0) },
+                { value: 0, color: ColorRGBA(255, 255, 255) },
+                { value: 50, color: ColorRGBA(255, 0, 0) },
+                { value: 100, color: ColorRGBA(0, 0, 255) }
             ],
-            interpolate: false,
-        });
+            interpolate: true // Interpolate between steps (which results in smooth color transitions, aka gradient)
+        })
 
-
-        createWaterDropDataGenerator()
-            .setRows(resolutionX)
-            .setColumns(resolutionY)
-            .generate()
-            .then((data) => {
+        // asychronously generate the data and assign it to the chart
+        generateData(resolutionX, resolutionY)
+            .then((data) => { // the asychronous step. generateData returns a promise
+                console.log(data) // log the data to the console
                 const heatmap = chart
-                    .addHeatmapGridSeries({
+                    .addHeatmapGridSeries({ // add a heatmap grid template to the chart
                         columns: resolutionX,
                         rows: resolutionY,
                         start: { x: 0, y: 0 },
@@ -52,13 +61,13 @@ const BasicSpectogram = () => {
                         dataOrder: "columns",
                     })
                     // Color Heatmap using previously created color look up table.
-                    .setFillStyle(new PalettedFill({ lut: palette }))
-                    .setWireframeStyle(emptyLine)
-                    .invalidateIntensityValues(data)
-                    .setMouseInteractions(false);
+                    .setFillStyle(new PalettedFill({ lut: palette })) // set the cahrts color palette
+                    .setWireframeStyle(emptyLine) // remove the wireframe (the grid lines)
+                    .invalidateIntensityValues(data) // THE ACTUAL DATA ASSIGNMENT to the chart
+                    .setMouseInteractions(false); // nessasary to enable the chart controls (zoom, pan, etc.)
 
                 // Add LegendBox.
-                const legend = chart.addLegendBox()
+                const legend = chart.addLegendBox() // the legend that shows the color palette
                     // Dispose example UI elements automatically if they take too much space. This is to avoid bad UI on mobile / etc. devices.
                     .setAutoDispose({
                         type: 'max-width',
@@ -68,11 +77,11 @@ const BasicSpectogram = () => {
             });
     }
 
-    // Call the function but make sure it only runs once
+    // call the generateChart function when the component is mounted (created)
     useEffect(() => {
-        generateData()
+        generateChart()
     }, [])
-    
+
 
     return (
         <div id="chart" style={{ width: '100%', height: '100%' }}></div>
