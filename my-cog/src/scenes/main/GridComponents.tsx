2104013d73f5e48070651706f2ddd7a0a68a6a59
Dekel-Eli <dekel56@gmail.com>
2023-03-12 21:04:58 +0200

    ...

--- my-cog/src/scenes/main/GridComponents.tsx
+++ my-cog/src/scenes/main/GridComponents.tsx
@@ -1,12 +1,11 @@
 import DataContainer from "../../components/DataContainer";
-import GraphinTest from "../../components/GraphinTest";
 import './GridStyle.css';
 
 export function Box (){
   return (
     <div className="box"
     style={{ margin: 0, height: '100%', paddingBottom: '40px' }}>
-      <GraphinTest/>
+      Somebody once told me the world is gonna roll me
     </div>
   );
 } 
