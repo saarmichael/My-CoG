db70c386b857b91d9e1df0d3575e7557c39bb11c
Dekel-Eli <dekel56@gmail.com>
2023-03-27 20:19:57 +0300

    added drop downs for topbar

--- my-cog/src/App.tsx
+++ my-cog/src/App.tsx
@@ -2,18 +2,13 @@ import './App.css';
 import Tabbing from './scenes/main/Tabs';
 import { ElectrodeFocusProvider } from './contexts/ElectrodeFocusContext';
 import DataContainer from './components/DataContainer';
-import { TopBar } from './scenes/main/TopBar';
-import { handleEditClick, handleFileClick, handleViewClick, handleOptionsClick, handleLogoutClick } from './shared/TopBarUtil';
+import { menuItems, TopBar } from './scenes/main/TopBar';
 
 function App() {
   return (
     <div>
        <TopBar
-        onFileClick={handleFileClick}
-        onEditClick={handleEditClick}
-        onViewClick={handleViewClick}
-        onOptionsClick={handleOptionsClick}
-        onLogoutClick={handleLogoutClick}
+        menuItems={menuItems}
       />
       <ElectrodeFocusProvider>
         <Tabbing />
