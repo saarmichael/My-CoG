cc98c5145e909ec15413e944996576f4fed4b093
Dekel-Eli <dekel56@gmail.com>
2023-06-09 12:37:29 +0300

    added cache to export data

--- PyCoG/export_data.py
+++ PyCoG/export_data.py
@@ -1,11 +1,30 @@
+from flask import session
 import numpy as np
+from db_write import write_calculation
+from server_config import Calculation
+from cache_check import data_in_db
 
 from coherence import coherence_time_frame
 from scipy.io import savemat
 
 
 def export_coherence_to_mat(name, data, sfreq, start, end, meta_data):
-    f, CM = coherence_time_frame(data, sfreq, start, end)
+    file_name = session["user_data_dir"]
+    url = "http://localhost:5000/connectivity?connectivity=coherence&start=" + str(start) + "&end=" + str(end)
+    cal = data_in_db(file_name=file_name, url=url, table=Calculation.query)
+    if cal:
+        print("Cached")
+        data = cal.data
+        f = data["f"]
+        CM = data["CM"]
+    else:
+        f, CM = coherence_time_frame(data, sfreq, start, end)
+        cal = Calculation(
+            file_name=file_name,
+            url=url,
+            data={"f": f, "CM": CM},
+        )
+        write_calculation(file_name=file_name, url=url, data={"f": f, "CM": CM}, created_by=session["username"])
     # write results to mat file
     full_name = name + ".mat"
     vars_dict = {
